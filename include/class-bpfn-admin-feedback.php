54150f19cd40cf7b108f414e649dbde07bdd33b9
Mohd Umar <pallavi@wbcomdesigns.com>
2021-10-29 19:04:27 +0530

    Fixed clear notification on activity visit issue

--- include/class-bpfn-admin-feedback.php
+++ include/class-bpfn-admin-feedback.php
@@ -293,7 +293,7 @@ if ( ! class_exists( 'Bpfn_Admin_Feedback' ) ) :
 		public function set_no_bug() {
 
 			// Bail out if not on correct page.
-			if ( ! isset( $_GET['_wpnonce'] ) || ( ! wp_verify_nonce( $_GET['_wpnonce'], 'bp-fav-notification-feedback-nounce' ) || ! is_admin() || ! isset( $_GET[ $this->nobug_option ] ) || ! current_user_can( 'manage_options' ) ) ) {
+			if ( ! isset( $_GET['_wpnonce'] ) || ( ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_GET['_wpnonce'] ) ), 'bp-fav-notification-feedback-nounce' ) || ! is_admin() || ! isset( $_GET[ $this->nobug_option ] ) || ! current_user_can( 'manage_options' ) ) ) {
 				return;
 			}
 
--- include/class-bpfn-functions.php
+++ include/class-bpfn-functions.php
@@ -60,25 +60,21 @@ if ( ! class_exists( 'BPFN_Functions' ) ) {
 		 * @author   Wbcom Designs
 		 */
 		public function wp_bp_activity_post_notification_mark( $content ) {
-			if ( is_single() ) {
-				// Only mark read if the current user is looking at his own mentions.
-				$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
-				if ( ! empty( $notification ) ) {
-					foreach ( $notification as $key => $value ) {
-						$href = bp_activity_get_permalink( $value->item_id );
-						$pobj = wp_parse_url( $href );
-						if ( isset( $pobj['query'] ) ) {
-							$obj = explode( '=', $pobj['query'] );
-							if ( 'p' === $obj[0] ) {
-								$pid = isset( $obj[1] ) ? $obj[1] : '';
-								if ( ! empty( $pid ) ) {
-									$current_notification_post = get_permalink( $pid );
-									$protocol                  = ( ! empty( filter_input( INPUT_SERVER, 'HTTPS' ) ) && filter_input( INPUT_SERVER, 'HTTPS' ) !== 'off' || filter_input( INPUT_SERVER, 'SERVER_PORT' ) === 443 ) ? 'https://' : 'http://';
-									$current_post              = esc_url( "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" );
-									if ( $current_notification_post == $current_post ) {
-										bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $value->component_name, $value->component_action, false );
-									}
-								}
+
+			if ( is_user_logged_in() ) {
+				$current_component = bp_current_component();
+				if ( $current_component && 'activity' === $current_component ) {
+					// Only mark read if the current user is looking at his own mentions.
+					$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
+					if ( ! empty( $notification ) ) {
+						foreach ( $notification as $key => $value ) {
+							$href              = bp_activity_get_permalink( $value->item_id );
+							$pobj              = wp_parse_url( $href );
+							$current_component = bp_current_component();
+							$action            = bp_current_action();
+							// Get the activity details.
+							if ( '' !== $action && $action === $value->item_id ) {
+								bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $value->component_name, $value->component_action, false );
 							}
 						}
 					}
