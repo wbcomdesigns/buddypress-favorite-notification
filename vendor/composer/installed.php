44d040670b63800959d17120d679c19b6d8fc350
Abhishek <pallavi@wbcomdesigns.com>
2023-11-27 16:42:03 +0530

    BP v12 Fixes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.2.2.x-dev',
+        'version' => '1.2.2.9999999-dev',
+        'reference' => '2c42dcfd7413d5b8c9312b35d8da9450044f7023',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.2.2.x-dev',
+            'version' => '1.2.2.9999999-dev',
+            'reference' => '2c42dcfd7413d5b8c9312b35d8da9450044f7023',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'hard-g/buddypress-12.0-url-polyfills' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => 'e61685bec96f2673b6b225510eec8b74c3d6a2cf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hard-g/buddypress-12.0-url-polyfills',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
