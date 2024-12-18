62801cf976b224d0ecf66af320b98fa7d17da4f4
Simran Kaur <simran.kaur@wbcomdesigns.in>
2024-12-18 18:11:03 +0530

    Fixed PHP warning while accessing activity timeline with buddyboss.
    https://3.basecamp.com/5798509/buckets/37595496/card_tables/cards/8133968813

--- include/class-bpfn-functions.php
+++ include/class-bpfn-functions.php
@@ -45,9 +45,17 @@ if ( ! class_exists( 'BPFN_Functions' ) ) {
 			if ( function_exists( 'bp_is_active' ) && bp_is_active( 'notifications' ) && function_exists( 'bp_notifications_get_notifications_for_user' ) ) {
 				$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
 				if ( ! empty( $notification ) ) {
-					foreach ( $notification as $key => $value ) {
-						if ( $activity->id === $value->item_id ) {
-							bp_notifications_mark_notification( $value->id, 0 );
+					if( function_exists( 'buddypress' ) && ! isset( buddypress()->buddyboss ) ){
+						foreach ( $notification as $key => $value ) {
+							if ( $activity->id === $value->item_id ) {
+								bp_notifications_mark_notification( $value->id, 0 );
+							}
+						}
+					} else {
+						$action = bp_current_action();
+						// Get the activity details.
+						if ( '' !== $action && $action === $notification->item_id ) {
+							bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $notification->component_name, $notification->component_action, false );
 						}
 					}
 				}
@@ -63,7 +71,7 @@ if ( ! class_exists( 'BPFN_Functions' ) ) {
 		 * @author   Wbcom Designs
 		 */
 		public function wp_bp_activity_post_notification_mark( $content ) {
-			
+
 			if ( is_user_logged_in() ) {
 				if ( function_exists( 'bp_is_active' ) && bp_is_active( 'notifications' ) && function_exists( 'bp_notifications_get_notifications_for_user' ) ) {
 					$current_component = bp_current_component();
@@ -71,13 +79,22 @@ if ( ! class_exists( 'BPFN_Functions' ) ) {
 						// Only mark read if the current user is looking at his own mentions.
 						$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
 						if ( ! empty( $notification ) ) {
-							foreach ( $notification as $key => $value ) {
+							if( function_exists( 'buddypress' ) && ! isset( buddypress()->buddyboss ) ){
+								foreach ( $notification as $key => $value ) {
+									$action = bp_current_action();
+									// Get the activity details.
+									if ( '' !== $action && $action === $value->item_id ) {
+										bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $value->component_name, $value->component_action, false );
+									}
+								}
+							} else {
 								$action = bp_current_action();
 								// Get the activity details.
-								if ( '' !== $action && $action === $value->item_id ) {
-									bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $value->component_name, $value->component_action, false );
+								if ( '' !== $action && $action === $notification->item_id ) {
+									bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $notification->component_name, $notification->component_action, false );
 								}
 							}
+							
 						}
 					}
 					
