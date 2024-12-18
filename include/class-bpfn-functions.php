30c53d4c0f18658b9a038e6d0629facc9bcddefa
Simran Kaur <simran.kaur@wbcomdesigns.in>
2024-12-18 17:22:54 +0530

    Fixed fatal error when notifications component is disabled.
    https://3.basecamp.com/5798509/buckets/37595496/card_tables/cards/8134631672

--- include/class-bpfn-functions.php
+++ include/class-bpfn-functions.php
@@ -42,14 +42,17 @@ if ( ! class_exists( 'BPFN_Functions' ) ) {
 			if ( empty( $activity->user_id ) || (int) bp_loggedin_user_id() !== (int) $activity->user_id ) {
 				return;
 			}
-			$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
-			if ( ! empty( $notification ) ) {
-				foreach ( $notification as $key => $value ) {
-					if ( $activity->id === $value->item_id ) {
-						bp_notifications_mark_notification( $value->id, 0 );
+			if ( function_exists( 'bp_is_active' ) && bp_is_active( 'notifications' ) && function_exists( 'bp_notifications_get_notifications_for_user' ) ) {
+				$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
+				if ( ! empty( $notification ) ) {
+					foreach ( $notification as $key => $value ) {
+						if ( $activity->id === $value->item_id ) {
+							bp_notifications_mark_notification( $value->id, 0 );
+						}
 					}
 				}
 			}
+			
 		}
 
 		/**
@@ -60,21 +63,24 @@ if ( ! class_exists( 'BPFN_Functions' ) ) {
 		 * @author   Wbcom Designs
 		 */
 		public function wp_bp_activity_post_notification_mark( $content ) {
-
+			
 			if ( is_user_logged_in() ) {
-				$current_component = bp_current_component();
-				if ( $current_component && 'activity' === $current_component ) {
-					// Only mark read if the current user is looking at his own mentions.
-					$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
-					if ( ! empty( $notification ) ) {
-						foreach ( $notification as $key => $value ) {
-							$action = bp_current_action();
-							// Get the activity details.
-							if ( '' !== $action && $action === $value->item_id ) {
-								bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $value->component_name, $value->component_action, false );
+				if ( function_exists( 'bp_is_active' ) && bp_is_active( 'notifications' ) && function_exists( 'bp_notifications_get_notifications_for_user' ) ) {
+					$current_component = bp_current_component();
+					if ( $current_component && 'activity' === $current_component ) {
+						// Only mark read if the current user is looking at his own mentions.
+						$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
+						if ( ! empty( $notification ) ) {
+							foreach ( $notification as $key => $value ) {
+								$action = bp_current_action();
+								// Get the activity details.
+								if ( '' !== $action && $action === $value->item_id ) {
+									bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $value->component_name, $value->component_action, false );
+								}
 							}
 						}
 					}
+					
 				}
 			}
 			return $content;
