5ce29cb21e2e7ca65a5a74b321c26bb694eb7fa0
Simran Kaur <simran.kaur@wbcomdesigns.in>
2024-12-19 14:28:10 +0530

    Removed unused function hooked to filter the content.

--- include/class-bpfn-functions.php
+++ include/class-bpfn-functions.php
@@ -26,7 +26,6 @@ if ( ! class_exists( 'BPFN_Functions' ) ) {
 		 */
 		public function __construct() {
 			add_action( 'bp_activity_screen_single_activity_permalink', array( $this, 'wb_bp_fav_activity_remove_screen_notifications' ), 10, 1 );
-			add_filter( 'the_content', array( $this, 'wp_bp_activity_post_notification_mark' ) );
 		}
 
 		/**
@@ -37,71 +36,28 @@ if ( ! class_exists( 'BPFN_Functions' ) ) {
 		 * @author   Wbcom Designs
 		 */
 		public function wb_bp_fav_activity_remove_screen_notifications( $activity ) {
+			
 			global $bp;
 			// Only mark read if the current user is looking at his own mentions.
 			if ( empty( $activity->user_id ) || (int) bp_loggedin_user_id() !== (int) $activity->user_id ) {
 				return;
 			}
-			if ( function_exists( 'bp_is_active' ) && bp_is_active( 'notifications' ) && function_exists( 'bp_notifications_get_notifications_for_user' ) ) {
-				$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
-				if ( ! empty( $notification ) ) {
-					if( function_exists( 'buddypress' ) && ! isset( buddypress()->buddyboss ) ){
-						foreach ( $notification as $key => $value ) {
-							if ( $activity->id === $value->item_id ) {
-								bp_notifications_mark_notification( $value->id, 0 );
-							}
-						}
-					} else {
-						$action = bp_current_action();
-						// Get the activity details.
-						if ( '' !== $action && $action === $notification->item_id ) {
-							bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $notification->component_name, $notification->component_action, false );
+			if ( function_exists( 'bp_is_active' ) && bp_is_active( 'notifications' ) && function_exists( 'bp_notifications_get_all_notifications_for_user' ) ) {
+				$notification = bp_notifications_get_all_notifications_for_user( bp_loggedin_user_id(), 'object' );
+				
+				if ( ! empty( $notification ) ) {					
+					foreach ( $notification as $key => $value ) {
+						if ( $activity->id === $value->item_id ) {
+							bp_notifications_mark_notification( $value->id, 0 );
+							
 						}
 					}
+				
 				}
 			}
 			
 		}
 
-		/**
-		 * Mark notifications as read when users visit their post activity page.
-		 *
-		 * @since 1.0.0
-		 * @param string $content The content.
-		 * @author   Wbcom Designs
-		 */
-		public function wp_bp_activity_post_notification_mark( $content ) {
-
-			if ( is_user_logged_in() ) {
-				if ( function_exists( 'bp_is_active' ) && bp_is_active( 'notifications' ) && function_exists( 'bp_notifications_get_notifications_for_user' ) ) {
-					$current_component = bp_current_component();
-					if ( $current_component && 'activity' === $current_component ) {
-						// Only mark read if the current user is looking at his own mentions.
-						$notification = bp_notifications_get_notifications_for_user( bp_loggedin_user_id(), 'object' );
-						if ( ! empty( $notification ) ) {
-							if( function_exists( 'buddypress' ) && ! isset( buddypress()->buddyboss ) ){
-								foreach ( $notification as $key => $value ) {
-									$action = bp_current_action();
-									// Get the activity details.
-									if ( '' !== $action && $action === $value->item_id ) {
-										bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $value->component_name, $value->component_action, false );
-									}
-								}
-							} else {
-								$action = bp_current_action();
-								// Get the activity details.
-								if ( '' !== $action && $action === $notification->item_id ) {
-									bp_notifications_mark_notifications_by_type( bp_loggedin_user_id(), $notification->component_name, $notification->component_action, false );
-								}
-							}
-							
-						}
-					}
-					
-				}
-			}
-			return $content;
-		}
 	}
 	new BPFN_Functions();
 }
